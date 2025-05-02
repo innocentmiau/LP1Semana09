6d6643931af0d5b5080e228f0addca7b2f92e4be
André <imandreleandro@gmail.com>
2025-05-02 10:01:22 +0100

    abcc

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace PlayerManager3
+{
+    public class Player
+    {
+        
+        public string Name { get; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return other.Score - this.Score;
+        }
+        
+    }
+}
