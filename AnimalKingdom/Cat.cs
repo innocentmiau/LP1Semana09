bdd31f32923c82c728af155c303ef72dc3052dac
André <imandreleandro@gmail.com>
2025-05-02 09:15:41 +0100

    wowowow

---
+++ AnimalKingdom/Cat.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimalKingdom
+{
+    public class Cat : Animal, IMammal
+    {
+        
+        public int NumberOfNipples { get; }
+
+        public Cat()
+        {
+            NumberOfNipples = 8;
+        }
+        
+        public override string Sound()
+        {
+            return base.Sound() + "Miau";
+        }
+    }
+}
---
+++ AnimalKingdom/Dog.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimalKingdom
+{
+    public class Dog : Animal, IMammal
+    {
+
+        public int NumberOfNipples { get; }
+
+        public Dog()
+        {
+            NumberOfNipples = 8;
+        }
+
+        public override string Sound()
+        {
+            return base.Sound() + "Woof!";
+        }
+    }
+}
